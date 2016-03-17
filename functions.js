a84c4780d3a3a4c57ef1122d163951b0d93f52cd
Herman Chan <hermchan89@gmail.com>
2016-03-16 21:41:14 -0700

    completed up to primes

--- functions.js
+++ functions.js
@@ -1,1 +1,26 @@
 // YOUR CODE HERE
+sillySum(arr)
+
+// Write a function that takes an array of numbers and returns the sum of each number multiplied by its index.
+function sillySum(arr) {
+  var count = 0;
+  for (var i = 0; count < arr.length; i ++) {
+    count += (count * arr[i]);
+  }
+}
+
+var myArray = [1, 2, 3];
+sillySum(myArray);
+
+
+// Create a function that returns true if the number passed in is a prime number and false if not.
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    } for (var i = 2; i < num; i++) {
+        if (num % i === 0){
+            return false;
+        }
+   } return true;
+}
+isPrime(7);
