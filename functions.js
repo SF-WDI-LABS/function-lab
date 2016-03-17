fa93184b2aa71304d4e5685fac13f5cbf1e61855
Herman Chan <hermchan89@gmail.com>
2016-03-16 23:31:41 -0700

    completed sillySum and isPrime

--- functions.js
+++ functions.js
@@ -24,3 +24,54 @@ function isPrime(num) {
    } return true;
 }
 isPrime(7);
+
+// Write a function called filter that filters elements of an array based on a callback function.
+// filter takes in an array and another function (a callback). The callback for filter will take in a number and return true or false (like isPrime does!). Your filter function should create a new array. The filter function should use the callback on each element of the original array. When the callback returns true for an element, filter should add that element to the new array. Make filter return the new array at the end!
+
+// starter structure for our filter function
+var filter = function(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    callback(arr[i])
+  }
+}
+filter ([0,1,2,3,4,5], console.log)
+// arr = [0,1,2,3,4,5]
+// callback = console.log
+//disregard information above, using for reference
+
+
+var filter = function(arr, callback) {
+function isPrime(num) {
+   if (num < 2) {
+        return false;
+   } for (var i = 2; i < num; i++) {
+        if (num % i === 0){
+            return false;
+       }
+     } return true;
+   }
+}
+
+var newArray = [];
+    if (isPrime()){
+    newArray.push(i));
+    }
+    for (var i = 0;i < newArray.length; i++) {
+        console.log(newArray.push(i));
+    }
+
+var isOdd = function(num) {
+    return num % 2 !== 0;
+}
+
+filter ([0,1,2,3,4,5], isOdd);
+// returns [1, 3, 5]
+
+// you could also try using your isPrime function as the callback:
+filter ([8,6,7,5,3,0,9], isPrime)
+// returns [7,5,3]
+
+var newArray = [];
+for (var i = 0; i < newArray.length; i++){
+  console.log(newArray(i));
+}
